e03b039a346a29629674cfacafede126014b6afc
yellove <yellove1992@gmail.com>
2016-02-24 15:45:24 +0900

    About page

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -6,6 +6,6 @@
     <h2 style="text-align: center">clockOS第一篇章: 走出矿山</h2>
     <br/>
     <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="/vid/poverty.mp4"></iframe>
+        <iframe class="embed-responsive-item" src="{{Clockos\Test::cdn('/vid/poverty.mp4')}}"></iframe>
     </div>
 @stop
